c31eb8b820fd9ea68e0501089cf6a9724381f6ee
for { kys() } <yyeet62425@gmail.com>
2025-09-11 13:52:16 +0530

    chore: use iterators

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,8 @@
-use minigrep::{search, search_case_insensitive};
+use minigrep::{Config, search, search_case_insensitive};
 use std::{env, error::Error, fs, process};
 
-pub struct Config {
-    pub query: String,
-    pub file_path: String,
-    pub ignore_case: bool,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
-
-        Ok(Config {
-            query,
-            file_path,
-            ignore_case,
-        })
-    }
-}
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
